b1cccca7accf9dff5281d40ec38057bd4fa68ce4
Sergio del Amo <sergio.delamo@softamo.com>
2025-02-25 17:09:19 +0100

    gh-pages

--- projectgen-bom/build.gradle.kts
+++
@@ -1,9 +0,0 @@
-plugins {
-    id("io.micronaut.build.internal.projectgen-base")
-    id("io.micronaut.build.internal.bom")
-}
-micronautBuild {
-    binaryCompatibility {
-        enabled = false
-    }
-}
