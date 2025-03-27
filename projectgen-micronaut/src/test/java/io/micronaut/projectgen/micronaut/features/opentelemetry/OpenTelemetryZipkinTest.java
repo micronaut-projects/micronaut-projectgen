caa356c2d4bf5c42a0a646eee09668135f9fb928
issam liqali <liqaliissam@gmail.com>
2025-03-27 11:57:22 +0000

    migrate tracing-opentelemetry-zipkin feature to use openrewrite

---
+++ projectgen-micronaut/src/test/java/io/micronaut/projectgen/micronaut/features/opentelemetry/OpenTelemetryZipkinTest.java
@@ -0,0 +1,53 @@
+package io.micronaut.projectgen.micronaut.features.opentelemetry;
+
+import io.micronaut.projectgen.core.io.MapOutputHandler;
+import io.micronaut.projectgen.micronaut.MicronautOptions;
+import io.micronaut.projectgen.micronaut.MicronautProjectGenerator;
+import io.micronaut.projectgen.test.BuildTestVerifier;
+import io.micronaut.projectgen.test.ConfigurationUtils;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+import java.util.Properties;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@MicronautTest(startApplication = false)
+class OpenTelemetryZipkinTest {
+
+    @Test
+    void zipkinConfiguration(MicronautProjectGenerator micronautProjectGenerator) throws Exception {
+        MicronautOptions options = MicronautOptions.builder().feature("tracing-opentelemetry-zipkin").build();
+        Map<String, String> project = generateProject(micronautProjectGenerator, options);
+        Properties applicationProperties = ConfigurationUtils.loadApplicationProperties(project);
+        assertEquals("zipkin", applicationProperties.getProperty("otel.traces.exporter"));
+    }
+
+    @Test
+    void zipkinFeaturesAddsTheDependency(MicronautProjectGenerator micronautProjectGenerator) throws Exception {
+        MicronautOptions options = MicronautOptions.builder().feature("tracing-opentelemetry-zipkin").build();
+        Map<String, String> project = generateProject(micronautProjectGenerator, options);
+        String buildGradle = project.get("build.gradle.kts");
+        assertNotNull(buildGradle);
+        BuildTestVerifier verifier = BuildTestVerifier.of(buildGradle, options);
+        assertTrue(verifier.hasDependency("io.opentelemetry", "opentelemetry-exporter-zipkin"),buildGradle);
+    }
+
+    @Test
+    void zipkinFeaturesAddsTheLinkInReadmeFile(MicronautProjectGenerator micronautProjectGenerator) throws Exception {
+        MicronautOptions options = MicronautOptions.builder().feature("tracing-opentelemetry-zipkin").build();
+        Map<String, String> project = generateProject(micronautProjectGenerator, options);
+        String readme = project.get("README.md");
+        assertNotNull(readme);
+        assertTrue(readme.contains("https://micronaut-projects.github.io/micronaut-tracing/latest/guide/#opentelemetry"));
+        assertTrue(readme.contains("https://opentelemetry.io"));
+    }
+
+    private static Map<String, String> generateProject(MicronautProjectGenerator micronautProjectGenerator,
+                                                       MicronautOptions options) throws Exception {
+        MapOutputHandler outputHandler = new MapOutputHandler();
+        micronautProjectGenerator.generate(options, outputHandler);
+        return outputHandler.getProject();
+    }
+}
