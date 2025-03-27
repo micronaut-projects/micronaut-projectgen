c72abba0c509b03570aa20339ad24012f031ef1f
issam liqali <liqaliissam@gmail.com>
2025-03-27 14:54:10 +0000

    migrate object storage local feature to use openrewrite

---
+++ projectgen-micronaut/src/test/java/io/micronaut/projectgen/micronaut/features/objectstorage/ObjectStorageLocalTest.java
@@ -0,0 +1,40 @@
+package io.micronaut.projectgen.micronaut.features.objectstorage;
+
+import io.micronaut.projectgen.core.io.MapOutputHandler;
+import io.micronaut.projectgen.micronaut.MicronautOptions;
+import io.micronaut.projectgen.micronaut.MicronautProjectGenerator;
+import io.micronaut.projectgen.test.BuildTestVerifier;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@MicronautTest(startApplication = false)
+class ObjectStorageLocalTest {
+    @Test
+    void objectStorageLocalFeaturesAddsTheDependency(MicronautProjectGenerator micronautProjectGenerator) throws Exception {
+        MicronautOptions options = MicronautOptions.builder().feature("object-storage-local").build();
+        Map<String, String> project = generateProject(micronautProjectGenerator, options);
+        String buildGradle = project.get("build.gradle.kts");
+        assertNotNull(buildGradle);
+        BuildTestVerifier verifier = BuildTestVerifier.of(buildGradle, options);
+        assertTrue(verifier.hasDependency("io.micronaut.objectstorage", "micronaut-object-storage-local"), buildGradle);
+    }
+
+    @Test
+    void objectStorageLocalFeaturesAddsTheLinkInReadmeFile(MicronautProjectGenerator micronautProjectGenerator) throws Exception {
+        MicronautOptions options = MicronautOptions.builder().feature("object-storage-local").build();
+        Map<String, String> project = generateProject(micronautProjectGenerator, options);
+        String readme = project.get("README.md");
+        assertNotNull(readme);
+        assertTrue(readme.contains("https://micronaut-projects.github.io/micronaut-object-storage/latest/guide/index.html#local"));
+    }
+
+    private static Map<String, String> generateProject(MicronautProjectGenerator micronautProjectGenerator,
+                                                       MicronautOptions options) throws Exception {
+        MapOutputHandler outputHandler = new MapOutputHandler();
+        micronautProjectGenerator.generate(options, outputHandler);
+        return outputHandler.getProject();
+    }
+}
