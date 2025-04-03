9551ed0939aad7ee91357a2edc5b702f82e2f922
issam liqali <63265720+lissam1@users.noreply.github.com>
2025-04-03 10:24:46 +0000

    migrate Multitenancy features to use openrewrite (#37)

---
+++ projectgen-micronaut/src/test/java/io/micronaut/projectgen/micronaut/features/multitenancy/MultitenancyTest.java
@@ -0,0 +1,41 @@
+package io.micronaut.projectgen.micronaut.features.multitenancy;
+
+import io.micronaut.projectgen.core.buildtools.Scope;
+import io.micronaut.projectgen.core.io.MapOutputHandler;
+import io.micronaut.projectgen.micronaut.MicronautOptions;
+import io.micronaut.projectgen.micronaut.MicronautProjectGenerator;
+import io.micronaut.projectgen.test.BuildTestVerifier;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@MicronautTest(startApplication = false)
+class MultitenancyTest {
+    @Test
+    void multitenancyFeaturesAddsTheDependency(MicronautProjectGenerator micronautProjectGenerator) throws Exception {
+        MicronautOptions options = MicronautOptions.builder().feature("multi-tenancy").build();
+        Map<String, String> project = generateProject(micronautProjectGenerator, options);
+        String buildGradle = project.get("build.gradle.kts");
+        assertNotNull(buildGradle);
+        BuildTestVerifier verifier = BuildTestVerifier.of(buildGradle, options);
+        assertTrue(verifier.hasDependency("io.micronaut.multitenancy", "micronaut-multitenancy", Scope.COMPILE),buildGradle);
+    }
+
+    @Test
+    void multitenancyFeaturesAddsTheLinkInReadmeFile(MicronautProjectGenerator micronautProjectGenerator) throws Exception {
+        MicronautOptions options = MicronautOptions.builder().feature("multi-tenancy").build();
+        Map<String, String> project = generateProject(micronautProjectGenerator, options);
+        String readme = project.get("README.md");
+        assertNotNull(readme);
+        assertTrue(readme.contains("https://docs.micronaut.io/latest/guide/index.html#multitenancy"));
+    }
+
+    private static Map<String, String> generateProject(MicronautProjectGenerator micronautProjectGenerator,
+                                                       MicronautOptions options) throws Exception {
+        MapOutputHandler outputHandler = new MapOutputHandler();
+        micronautProjectGenerator.generate(options, outputHandler);
+        return outputHandler.getProject();
+    }
+}
