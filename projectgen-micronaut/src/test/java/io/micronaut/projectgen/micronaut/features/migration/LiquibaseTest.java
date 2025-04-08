289c210062d318d04c403728c68301f0f065d9f0
Sergio del Amo <sergio.delamo@softamo.com>
2025-04-08 12:36:37 +0200

    addTemplatesByRecipeName

--- projectgen-micronaut/src/test/java/io/micronaut/projectgen/micronaut/features/migration/LiquibaseTest.java
+++ projectgen-micronaut/src/test/java/io/micronaut/projectgen/micronaut/features/migration/LiquibaseTest.java
@@ -1,12 +1,12 @@
 package io.micronaut.projectgen.micronaut.features.migration;
 
-import io.micronaut.core.util.StringUtils;
 import io.micronaut.projectgen.core.buildtools.Scope;
 import io.micronaut.projectgen.core.io.MapOutputHandler;
 import io.micronaut.projectgen.micronaut.MicronautOptions;
 import io.micronaut.projectgen.micronaut.MicronautProjectGenerator;
 import io.micronaut.projectgen.test.BuildTestVerifier;
 import io.micronaut.projectgen.test.ConfigurationUtils;
+import io.micronaut.projectgen.test.StringUtils;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
 import org.junit.jupiter.api.Test;
 import java.util.Map;
@@ -22,7 +22,39 @@ class LiquibaseTest {
         MicronautOptions options = MicronautOptions.builder().feature("liquibase").build();
         Map<String, String> project = generateProject(micronautProjectGenerator, options);
         Properties applicationProperties = ConfigurationUtils.loadApplicationProperties(project);
-        assertEquals("classpath:db/liquibase-changelog.xml", applicationProperties.getProperty("liquibase.datasources.default.change-log"));
+        assertEquals("classpath:db/liquibase-changelog.xml",
+            applicationProperties.getProperty("liquibase.datasources.default.change-log"));
+    }
+
+    @Test
+    void liquibaseChangeset(MicronautProjectGenerator micronautProjectGenerator) throws Exception {
+        MicronautOptions options = MicronautOptions.builder().feature("liquibase").build();
+        Map<String, String> project = generateProject(micronautProjectGenerator, options);
+        String liquibaseChangelogXml = project.get("src/main/resources/db/liquibase-changelog.xml");
+        assertNotNull(liquibaseChangelogXml);
+        assertEquals("""
+        <?xml version="1.0" encoding="UTF-8" ?>
+        <databaseChangeLog
+          xmlns="http://www.liquibase.org/xml/ns/dbchangelog"
+          xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+          xsi:schemaLocation="http://www.liquibase.org/xml/ns/dbchangelog
+                 http://www.liquibase.org/xml/ns/dbchangelog/dbchangelog-3.1.xsd">
+          <include file="changelog/01-schema.xml" relativeToChangelogFile="true"/>
+        </databaseChangeLog>
+        """, liquibaseChangelogXml);
+        String schemaXml = project.get("src/main/resources/db/changelog/01-schema.xml");
+        assertNotNull(schemaXml);
+        assertEquals("""
+            <?xml version="1.0" encoding="UTF-8" ?>
+            <databaseChangeLog
+              xmlns="http://www.liquibase.org/xml/ns/dbchangelog"
+              xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+              xsi:schemaLocation="http://www.liquibase.org/xml/ns/dbchangelog
+                     http://www.liquibase.org/xml/ns/dbchangelog/dbchangelog-3.1.xsd">
+              <changeSet id="01" author="username">
+              </changeSet>
+            </databaseChangeLog>
+            """, schemaXml);
     }
 
     @Test
@@ -33,6 +65,7 @@ class LiquibaseTest {
         assertNotNull(buildGradle);
         BuildTestVerifier verifier = BuildTestVerifier.of(buildGradle, options);
         assertTrue(verifier.hasDependency("io.micronaut.liquibase", "micronaut-liquibase", Scope.COMPILE),buildGradle);
+        assertTrue(verifier.hasDependency("org.slf4j", "jul-to-slf4j", Scope.COMPILE),buildGradle);
     }
 
     @Test
