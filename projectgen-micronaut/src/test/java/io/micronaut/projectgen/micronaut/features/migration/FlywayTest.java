e8748cb38808e7fc08b805e9e3b2001999954d7a
Sergio del Amo <sergio.delamo@softamo.com>
2025-04-08 11:38:22 +0200

    add test for flyway database dependencies

--- projectgen-micronaut/src/test/java/io/micronaut/projectgen/micronaut/features/migration/FlywayTest.java
+++ projectgen-micronaut/src/test/java/io/micronaut/projectgen/micronaut/features/migration/FlywayTest.java
@@ -1,25 +1,39 @@
 package io.micronaut.projectgen.micronaut.features.migration;
 
 import io.micronaut.core.util.StringUtils;
+import io.micronaut.projectgen.core.buildtools.Phase;
 import io.micronaut.projectgen.core.buildtools.Scope;
+import io.micronaut.projectgen.core.buildtools.Source;
+import io.micronaut.projectgen.core.buildtools.maven.MavenScope;
 import io.micronaut.projectgen.core.io.MapOutputHandler;
+import io.micronaut.projectgen.core.options.Language;
 import io.micronaut.projectgen.micronaut.MicronautOptions;
 import io.micronaut.projectgen.micronaut.MicronautProjectGenerator;
 import io.micronaut.projectgen.test.BuildTestVerifier;
 import io.micronaut.projectgen.test.ConfigurationUtils;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import jakarta.inject.Inject;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.Properties;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @MicronautTest(startApplication = false)
 class FlywayTest {
 
+    @Inject
+    MicronautProjectGenerator micronautProjectGenerator;
+
     @Test
-    void flywayConfiguration(MicronautProjectGenerator micronautProjectGenerator) throws Exception {
+    void flywayConfiguration() throws Exception {
         MicronautOptions options = MicronautOptions.builder().feature("flyway").build();
         Map<String, String> project = generateProject(micronautProjectGenerator, options);
         Properties applicationProperties = ConfigurationUtils.loadApplicationProperties(project);
@@ -27,13 +41,37 @@ class FlywayTest {
     }
 
     @Test
-    void flywayFeaturesAddsTheDependency(MicronautProjectGenerator micronautProjectGenerator) throws Exception {
+    void flywayFeaturesAddsTheDependency() throws Exception {
         MicronautOptions options = MicronautOptions.builder().feature("flyway").build();
         Map<String, String> project = generateProject(micronautProjectGenerator, options);
         String buildGradle = project.get("build.gradle.kts");
         assertNotNull(buildGradle);
         BuildTestVerifier verifier = BuildTestVerifier.of(buildGradle, options);
         assertTrue(verifier.hasDependency("io.micronaut.flyway", "micronaut-flyway", Scope.COMPILE), buildGradle);
+    }
+
+    @ParameterizedTest
+    @MethodSource("flywayDependencies")
+    void testProjectNaturalName(String groupId, String artifactId, String feature) throws Exception {
+        MicronautOptions options = MicronautOptions.builder()
+            .feature("flyway")
+            .feature(feature)
+            .build();
+        Map<String, String> project = generateProject(micronautProjectGenerator, options);
+        String buildGradle = project.get("build.gradle.kts");
+        assertNotNull(buildGradle);
+        BuildTestVerifier verifier = BuildTestVerifier.of(buildGradle, options);
+        assertTrue(verifier.hasDependency(groupId, artifactId, Scope.RUNTIME), buildGradle);
+    }
+
+    private static Stream<Arguments> flywayDependencies() {
+        return Stream.of(
+            Arguments.of("org.flywaydb", "flyway-sqlserver", "sqlserver"),
+            Arguments.of("org.flywaydb", "flyway-mysql", "mysql"),
+            Arguments.of("org.flywaydb", "flyway-mysql", "mariadb"),
+            Arguments.of("org.flywaydb", "flyway-database-postgresql", "postgres"),
+            Arguments.of("org.flywaydb", "flyway-database-oracle", "oracle")
+        );
     }
 
     @Test
