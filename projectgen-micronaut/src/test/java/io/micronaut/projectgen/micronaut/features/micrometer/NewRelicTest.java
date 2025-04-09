fbe92200a9f903e1c73f40407c9e64be01cba886
issam liqali <liqaliissam@gmail.com>
2025-04-09 17:05:03 +0100

    migrate new relic feature to use openrewrite

---
+++ projectgen-micronaut/src/test/java/io/micronaut/projectgen/micronaut/features/micrometer/NewRelicTest.java
@@ -0,0 +1,47 @@
+package io.micronaut.projectgen.micronaut.features.micrometer;
+
+import io.micronaut.core.util.StringUtils;
+import io.micronaut.projectgen.core.buildtools.Scope;
+import io.micronaut.projectgen.core.io.MapOutputHandler;
+import io.micronaut.projectgen.micronaut.MicronautOptions;
+import io.micronaut.projectgen.micronaut.MicronautProjectGenerator;
+import io.micronaut.projectgen.test.BuildTestVerifier;
+import io.micronaut.projectgen.test.ConfigurationUtils;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import java.util.Map;
+import java.util.Properties;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@MicronautTest(startApplication = false)
+class NewRelicTest {
+    @Test
+    void newRelicConfiguration(MicronautProjectGenerator micronautProjectGenerator) throws Exception {
+        MicronautOptions options = MicronautOptions.builder().feature("micrometer-new-relic").build();
+        Map<String, String> project = generateProject(micronautProjectGenerator, options);
+        Properties applicationProperties = ConfigurationUtils.loadApplicationProperties(project);
+        assertEquals(StringUtils.TRUE, applicationProperties.getProperty("micronaut.metrics.export.new-relic.enabled"));
+        assertEquals(StringUtils.TRUE, applicationProperties.getProperty("micronaut.metrics.enabled"));
+        assertEquals("PT1M", applicationProperties.getProperty("micronaut.metrics.export.new-relic.step"));
+        assertEquals("${NEWRELIC_API_KEY}", applicationProperties.getProperty("micronaut.metrics.export.newrelic.apiKey"));
+        assertEquals("${NEWRELIC_ACCOUNT_ID}", applicationProperties.getProperty("micronaut.metrics.export.newrelic.accountId"));
+    }
+
+    @Test
+    void humioFeaturesAddsTheDependency(MicronautProjectGenerator micronautProjectGenerator) throws Exception {
+        MicronautOptions options = MicronautOptions.builder().feature("micrometer-new-relic").build();
+        Map<String, String> project = generateProject(micronautProjectGenerator, options);
+        String buildGradle = project.get("build.gradle.kts");
+        assertNotNull(buildGradle);
+        BuildTestVerifier verifier = BuildTestVerifier.of(buildGradle, options);
+        assertTrue(verifier.hasDependency("io.micronaut.micrometer", "micronaut-micrometer-registry-new-relic", Scope.COMPILE), buildGradle);
+    }
+
+    private static Map<String, String> generateProject(MicronautProjectGenerator micronautProjectGenerator,
+                                                       MicronautOptions options) throws Exception {
+        MapOutputHandler outputHandler = new MapOutputHandler();
+        micronautProjectGenerator.generate(options, outputHandler);
+        return outputHandler.getProject();
+    }
+}
