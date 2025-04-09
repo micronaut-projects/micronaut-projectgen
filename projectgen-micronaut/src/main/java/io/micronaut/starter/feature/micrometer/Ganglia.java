cea9a9cb596532f045eb58d40e8a91156d8344e8
issam liqali <liqaliissam@gmail.com>
2025-04-09 15:49:23 +0100

    migrate ganglia feature to use openrewrite

--- projectgen-micronaut/src/main/java/io/micronaut/starter/feature/micrometer/Ganglia.java
+++ projectgen-micronaut/src/main/java/io/micronaut/starter/feature/micrometer/Ganglia.java
@@ -18,12 +18,15 @@ package io.micronaut.starter.feature.micrometer;
 import io.micronaut.context.annotation.Requires;
 import io.micronaut.core.util.StringUtils;
 import io.micronaut.projectgen.core.generator.GeneratorContext;
+import io.micronaut.projectgen.core.openrewrite.OpenRewriteFeature;
 import io.micronaut.starter.feature.other.Management;
 import jakarta.inject.Singleton;
 
+import java.util.List;
+
 @Requires(property = "micronaut.starter.feature.micrometer.ganglia.enabled", value = StringUtils.TRUE, defaultValue = StringUtils.TRUE)
 @Singleton
-public class Ganglia extends MicrometerFeature implements MicrometerRegistryFeature {
+public class Ganglia extends MicrometerFeature implements MicrometerRegistryFeature, OpenRewriteFeature {
 
     public Ganglia(Core core, Management management) {
         super(core, management);
@@ -35,10 +38,8 @@ public class Ganglia extends MicrometerFeature implements MicrometerRegistryFeat
     }
 
     @Override
-    public void addConfiguration(GeneratorContext generatorContext) {
-        generatorContext.getConfiguration().put(EXPORT_PREFIX + ".ganglia.enabled", true);
-        generatorContext.getConfiguration().put(EXPORT_PREFIX + ".ganglia.protocolVersion", 3.1);
-        generatorContext.getConfiguration().put(EXPORT_PREFIX + ".ganglia.step", "PT1M");
+    public List<String> getRecipes(GeneratorContext generatorContext) {
+        return List.of("io.micronaut.starter.feature.micrometer-ganglia");
     }
 
     @Override
