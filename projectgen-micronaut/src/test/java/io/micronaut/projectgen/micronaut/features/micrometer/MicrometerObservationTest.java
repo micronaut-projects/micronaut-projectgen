141c1de55f884d4caed03ca30baff9e758055815
issam liqali <liqaliissam@gmail.com>
2025-04-09 16:43:36 +0100

    migrate micrometer observation feature to use openrewrite

---
+++ projectgen-micronaut/src/test/java/io/micronaut/projectgen/micronaut/features/micrometer/MicrometerObservationTest.java
@@ -0,0 +1,43 @@
+package io.micronaut.projectgen.micronaut.features.micrometer;
+
+import io.micronaut.core.util.StringUtils;
+import io.micronaut.projectgen.core.buildtools.Scope;
+import io.micronaut.projectgen.core.io.MapOutputHandler;
+import io.micronaut.projectgen.micronaut.MicronautOptions;
+import io.micronaut.projectgen.micronaut.MicronautProjectGenerator;
+import io.micronaut.projectgen.test.BuildTestVerifier;
+import io.micronaut.projectgen.test.ConfigurationUtils;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import java.util.Map;
+import java.util.Properties;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@MicronautTest(startApplication = false)
+class MicrometerObservationTest {
+    @Test
+    void micrometerObservationonfiguration(MicronautProjectGenerator micronautProjectGenerator) throws Exception {
+        MicronautOptions options = MicronautOptions.builder().feature("micrometer-observation").build();
+        Map<String, String> project = generateProject(micronautProjectGenerator, options);
+        Properties applicationProperties = ConfigurationUtils.loadApplicationProperties(project);
+        assertEquals(StringUtils.TRUE, applicationProperties.getProperty("micronaut.metrics.enabled"));
+    }
+
+    @Test
+    void micrometerObservationFeaturesAddsTheDependency(MicronautProjectGenerator micronautProjectGenerator) throws Exception {
+        MicronautOptions options = MicronautOptions.builder().feature("micrometer-observation").build();
+        Map<String, String> project = generateProject(micronautProjectGenerator, options);
+        String buildGradle = project.get("build.gradle.kts");
+        assertNotNull(buildGradle);
+        BuildTestVerifier verifier = BuildTestVerifier.of(buildGradle, options);
+        assertTrue(verifier.hasDependency("io.micronaut.micrometer", "micronaut-micrometer-observation", Scope.COMPILE), buildGradle);
+    }
+
+    private static Map<String, String> generateProject(MicronautProjectGenerator micronautProjectGenerator,
+                                                       MicronautOptions options) throws Exception {
+        MapOutputHandler outputHandler = new MapOutputHandler();
+        micronautProjectGenerator.generate(options, outputHandler);
+        return outputHandler.getProject();
+    }
+}
