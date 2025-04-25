ac473cde6e003a69e023baa0675d24cff0e26660
issam liqali <63265720+lissam1@users.noreply.github.com>
2025-04-25 12:34:39 +0100

    redis lettuce feature (#80)
    Co-authored-by: Sergio del Amo <sergio.delamo@softamo.com>

--- projectgen-micronaut/src/main/java/io/micronaut/starter/feature/redis/RedisLettuce.java
+++ projectgen-micronaut/src/main/java/io/micronaut/starter/feature/redis/RedisLettuce.java
@@ -17,18 +17,17 @@ package io.micronaut.starter.feature.redis;
 
 import io.micronaut.context.annotation.Requires;
 import io.micronaut.core.util.StringUtils;
-import io.micronaut.projectgen.core.generator.ModuleContext;
-import io.micronaut.projectgen.micronaut.ApplicationType;
+import io.micronaut.projectgen.core.openrewrite.OpenRewriteFeature;
 import io.micronaut.projectgen.core.generator.GeneratorContext;
-import io.micronaut.projectgen.core.buildtools.dependencies.Dependency;
 import io.micronaut.starter.feature.Category;
-import io.micronaut.projectgen.core.feature.Feature;
 
 import jakarta.inject.Singleton;
 
+import java.util.List;
+
 @Requires(property = "micronaut.starter.feature.redis.lettuce.enabled", value = StringUtils.TRUE, defaultValue = StringUtils.TRUE)
 @Singleton
-public class RedisLettuce implements Feature {
+public class RedisLettuce implements OpenRewriteFeature {
 
     @Override
     public String getName() {
@@ -46,13 +45,8 @@ public class RedisLettuce implements Feature {
     }
 
     @Override
-    public void apply(GeneratorContext generatorContext) {
-        ModuleContext module = generatorContext.getRootModule();
-        module.configuration().put("redis.uri", "redis://localhost");
-        module.addDependency(Dependency.builder()
-                .groupId("io.micronaut.redis")
-                .artifactId("micronaut-redis-lettuce")
-                .compile());
+    public List<String> getRecipes(GeneratorContext generatorContext) {
+        return List.of("io.micronaut.starter.feature.redis-lettuce");
     }
 
     @Override
