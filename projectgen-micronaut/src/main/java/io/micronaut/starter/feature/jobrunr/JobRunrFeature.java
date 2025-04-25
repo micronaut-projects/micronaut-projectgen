b42b9e4082c19aa187d1b5b582e142a8932f7b6a
issam liqali <63265720+lissam1@users.noreply.github.com>
2025-04-25 12:40:49 +0100

    jobrunr feature (#74)
    Co-authored-by: Sergio del Amo <sergio.delamo@softamo.com>

--- projectgen-micronaut/src/main/java/io/micronaut/starter/feature/jobrunr/JobRunrFeature.java
+++ projectgen-micronaut/src/main/java/io/micronaut/starter/feature/jobrunr/JobRunrFeature.java
@@ -18,6 +18,7 @@ package io.micronaut.starter.feature.jobrunr;
 import io.micronaut.context.annotation.Requires;
 import io.micronaut.core.annotation.Nullable;
 import io.micronaut.core.util.StringUtils;
+import io.micronaut.projectgen.core.openrewrite.OpenRewriteFeature;
 import io.micronaut.projectgen.core.feature.config.Configuration;
 import io.micronaut.projectgen.core.generator.ModuleContext;
 import io.micronaut.projectgen.micronaut.ApplicationType;
@@ -28,17 +29,11 @@ import io.micronaut.starter.feature.MicronautCommunityFeature;
 import io.micronaut.starter.options.MicronautVersion;
 import jakarta.inject.Singleton;
 
+import java.util.List;
+
 @Requires(property = "micronaut.starter.feature.jobrunr.jobrunr.enabled", value = StringUtils.TRUE, defaultValue = StringUtils.TRUE)
 @Singleton
-public class JobRunrFeature implements MicronautCommunityFeature {
-    private static final String JOBRUNR_ARTIFACT_ID = "jobrunr-micronaut-feature";
-
-    private static final Dependency JOBRUNR_DEPENDENCY = Dependency.builder()
-            .lookupArtifactId(JOBRUNR_ARTIFACT_ID)
-            .compile()
-            .build();
-    private static final String PROPERTY_JOBRUNR_BACKGROUND_JOB_SERVER_ENABLED = "jobrunr.background-job-server.enabled";
-    private static final String PROPERTY_JOBRUNR_DASHBOARD_ENABLED = "jobrunr.dashboard.enabled";
+public class JobRunrFeature implements MicronautCommunityFeature, OpenRewriteFeature {
 
     @Override
     public MicronautVersion builtWithMicronautVersion() {
@@ -48,6 +43,11 @@ public class JobRunrFeature implements MicronautCommunityFeature {
     @Override
     public String getCommunityFeatureTitle() {
         return "JobRunr Integration for Micronaut";
+    }
+
+    @Override
+    public String getName() {
+        return "jobrunr";
     }
 
     @Override
@@ -72,24 +72,7 @@ public class JobRunrFeature implements MicronautCommunityFeature {
     }
 
     @Override
-    public void apply(GeneratorContext generatorContext) {
-        ModuleContext module = generatorContext.getRootModule();
-        addDependencies(module);
-        addConfiguration(module);
-    }
-
-    protected void addDependencies(ModuleContext module) {
-        module.addDependency(JOBRUNR_DEPENDENCY);
-    }
-
-    protected void addConfiguration(ModuleContext module) {
-        Configuration configuration = module.configuration();
-        configuration.put(PROPERTY_JOBRUNR_BACKGROUND_JOB_SERVER_ENABLED, false);
-        configuration.put(PROPERTY_JOBRUNR_DASHBOARD_ENABLED, false);
-    }
-
-    @Override
-    public String getThirdPartyDocumentation(GeneratorContext generatorContext) {
-        return "https://www.jobrunr.io/en/documentation/configuration/micronaut/";
+    public List<String> getRecipes(GeneratorContext generatorContext) {
+        return List.of("io.micronaut.starter.feature.jobrunr");
     }
 }
