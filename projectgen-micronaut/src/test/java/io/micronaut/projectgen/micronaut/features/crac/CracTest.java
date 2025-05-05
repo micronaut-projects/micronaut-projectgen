aa287de29fa8fde5e126552ff089732b4282b290
issam liqali <liqaliissam@gmail.com>
2025-05-05 15:40:45 +0100

    crac feature

---
+++ projectgen-micronaut/src/test/java/io/micronaut/projectgen/micronaut/features/crac/CracTest.java
@@ -0,0 +1,44 @@
+package io.micronaut.projectgen.micronaut.features.crac;
+
+import io.micronaut.projectgen.core.buildtools.Scope;
+import io.micronaut.projectgen.core.io.MapOutputHandler;
+import io.micronaut.projectgen.micronaut.MicronautOptions;
+import io.micronaut.projectgen.micronaut.MicronautProjectGenerator;
+import io.micronaut.projectgen.test.BuildTestVerifier;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@MicronautTest(startApplication = false)
+class CracTest {
+    @Test
+    void cracFeaturesAddsTheDependency(MicronautProjectGenerator micronautProjectGenerator) throws Exception {
+        MicronautOptions options = MicronautOptions.builder().feature("crac").build();
+        Map<String, String> project = generateProject(micronautProjectGenerator, options);
+        String buildGradle = project.get("build.gradle.kts");
+        assertNotNull(buildGradle);
+        BuildTestVerifier verifier = BuildTestVerifier.of(buildGradle, options);
+        assertTrue(verifier.hasDependency("io.micronaut.crac", "micronaut-crac", Scope.COMPILE), buildGradle);
+//        assertTrue(verifier.hasBuildPlugin("io.micronaut.crac"), buildGradle);
+    }
+
+    @Test
+    void cracFeaturesAddsTheLinkInReadmeFile(MicronautProjectGenerator micronautProjectGenerator) throws Exception {
+        MicronautOptions options = MicronautOptions.builder().feature("crac").build();
+        Map<String, String> project = generateProject(micronautProjectGenerator, options);
+        String readme = project.get("README.md");
+        assertNotNull(readme);
+        assertTrue(readme.contains("https://micronaut-projects.github.io/micronaut-crac/latest/guide"));
+        assertTrue(readme.contains("https://wiki.openjdk.org/display/CRaC"));
+
+    }
+
+    private static Map<String, String> generateProject(MicronautProjectGenerator micronautProjectGenerator,
+                                                       MicronautOptions options) throws Exception {
+        MapOutputHandler outputHandler = new MapOutputHandler();
+        micronautProjectGenerator.generate(options, outputHandler);
+        return outputHandler.getProject();
+    }
+}
