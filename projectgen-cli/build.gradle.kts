c2f8aaa1a89dac361f8fd1f757f78a1145ebfbcd
Sergio del Amo <sergio.delamo@softamo.com>
2025-05-07 16:28:47 +0200

    projectgen-cli (#124)
    * projectgen-cli
    
    * add test for exception

---
+++ projectgen-cli/build.gradle.kts
@@ -0,0 +1,10 @@
+plugins {
+    id("io.micronaut.build.internal.projectgen-module")
+}
+dependencies {
+    implementation(project(":micronaut-projectgen-core"))
+    implementation(mnPicocli.picocli)
+    testAnnotationProcessor(mn.micronaut.inject.java)
+    testImplementation(mnTest.micronaut.test.junit5)
+    testRuntimeOnly(mnTest.junit.jupiter.engine)
+}
