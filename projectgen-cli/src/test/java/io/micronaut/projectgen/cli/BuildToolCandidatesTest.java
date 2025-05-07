3dda86158d0c4739858efc1e64b62fe031f56ddf
Sergio del Amo <sergio.delamo@softamo.com>
2025-05-07 16:25:21 +0200

    projectgen-cli

---
+++ projectgen-cli/src/test/java/io/micronaut/projectgen/cli/BuildToolCandidatesTest.java
@@ -0,0 +1,16 @@
+package io.micronaut.projectgen.cli;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BuildToolCandidatesTest {
+
+    @Test
+    void buildToolCandidates() {
+        assertEquals(List.of("gradle", "gradle_kotlin", "maven"),
+            new BuildToolCandidates());
+    }
+}
---
+++ projectgen-cli/src/test/java/io/micronaut/projectgen/cli/BuildToolConverterTest.java
@@ -0,0 +1,17 @@
+package io.micronaut.projectgen.cli;
+
+import io.micronaut.projectgen.core.buildtools.BuildTool;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BuildToolConverterTest {
+
+    @Test
+    void buildToolConverter() throws Exception {
+        BuildToolConverter converter = new BuildToolConverter();
+        assertEquals(BuildTool.GRADLE, converter.convert("gradle"));
+        assertEquals(BuildTool.GRADLE_KOTLIN, converter.convert("gradle_kotlin"));
+        assertEquals(BuildTool.MAVEN, converter.convert("maven"));
+    }
+}
