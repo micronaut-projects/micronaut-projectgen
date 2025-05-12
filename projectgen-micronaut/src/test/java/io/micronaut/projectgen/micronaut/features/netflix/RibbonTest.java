8afbcdd97f03c172120eca4b5c2feb3af1262187
issam liqali <liqaliissam@gmail.com>
2025-05-12 16:53:17 +0100

    netflix ribbon feature

---
+++ projectgen-micronaut/src/test/java/io/micronaut/projectgen/micronaut/features/netflix/RibbonTest.java
@@ -0,0 +1,58 @@
+package io.micronaut.projectgen.micronaut.features.netflix;
+
+import io.micronaut.core.util.StringUtils;
+import io.micronaut.projectgen.core.buildtools.Scope;
+import io.micronaut.projectgen.core.generator.ProjectGenerator;
+import io.micronaut.projectgen.core.io.MapOutputHandler;
+import io.micronaut.projectgen.core.options.GenericOptionsBuilder;
+import io.micronaut.projectgen.core.options.Options;
+import io.micronaut.projectgen.micronaut.ApplicationType;
+import io.micronaut.projectgen.micronaut.OptionsFixture;
+import io.micronaut.projectgen.test.BuildTestVerifier;
+import io.micronaut.projectgen.test.ConfigurationUtils;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@MicronautTest(startApplication = false)
+class RibbonTest {
+    @Test
+    void ribbonConfiguration(ProjectGenerator micronautProjectGenerator) throws Exception {
+        Options options = OptionsFixture.defaultGradle().features(List.of("netflix-ribbon")).build();
+        Map<String, String> project = generateProject(micronautProjectGenerator, options);
+        Properties applicationProperties = ConfigurationUtils.loadApplicationProperties(project);
+        assertEquals("test", applicationProperties.getProperty("ribbon.VipAddress"));
+        assertEquals("2000", applicationProperties.getProperty("ribbon.ServerListRefreshInterval"));
+    }
+
+    @Test
+    void ribbonFeaturesAddsTheDependency(ProjectGenerator micronautProjectGenerator) throws Exception {
+        Options options = OptionsFixture.defaultGradle().features(List.of("netflix-ribbon")).build();
+        Map<String, String> project = generateProject(micronautProjectGenerator, options);
+        String buildGradle = project.get("build.gradle.kts");
+        assertNotNull(buildGradle);
+        BuildTestVerifier verifier = BuildTestVerifier.of(buildGradle, options);
+        assertTrue(verifier.hasDependency("io.micronaut.netflix", "micronaut-netflix-ribbon", Scope.COMPILE), buildGradle);
+    }
+
+    @Test
+    void ribbonFeaturesAddsTheLinkInReadmeFile(ProjectGenerator micronautProjectGenerator) throws Exception {
+        Options options = OptionsFixture.defaultGradle().features(List.of("netflix-ribbon")).build();
+        Map<String, String> project = generateProject(micronautProjectGenerator, options);
+        String readme = project.get("README.md");
+        assertNotNull(readme);
+        assertTrue(readme.contains("https://docs.micronaut.io/latest/guide/index.html#netflixRibbon"));
+    }
+
+    private static Map<String, String> generateProject(ProjectGenerator micronautProjectGenerator,
+                                                       Options options) throws Exception {
+        MapOutputHandler outputHandler = new MapOutputHandler();
+        micronautProjectGenerator.generate(options, outputHandler);
+        return outputHandler.getProject();
+    }
+}
