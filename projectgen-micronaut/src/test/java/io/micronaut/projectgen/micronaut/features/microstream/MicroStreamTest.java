afb0b6a9734ecfce9ee797adf3003fc8f4d329fa
issam liqali <liqaliissam@gmail.com>
2025-05-12 15:38:02 +0100

    microstream feature

---
+++ projectgen-micronaut/src/test/java/io/micronaut/projectgen/micronaut/features/microstream/MicroStreamTest.java
@@ -0,0 +1,47 @@
+package io.micronaut.projectgen.micronaut.features.microstream;
+
+import io.micronaut.projectgen.core.buildtools.Scope;
+import io.micronaut.projectgen.core.generator.ProjectGenerator;
+import io.micronaut.projectgen.core.io.MapOutputHandler;
+import io.micronaut.projectgen.core.options.Options;
+import io.micronaut.projectgen.micronaut.OptionsFixture;
+import io.micronaut.projectgen.test.BuildTestVerifier;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@MicronautTest(startApplication = false)
+class MicroStreamTest {
+    @Test
+    void microstreamFeaturesAddsTheDependency(ProjectGenerator micronautProjectGenerator) throws Exception {
+        Options options = OptionsFixture.defaultGradle().features(List.of("microstream")).build();
+        Map<String, String> project = generateProject(micronautProjectGenerator, options);
+        String buildGradle = project.get("build.gradle.kts");
+        assertNotNull(buildGradle);
+        BuildTestVerifier verifier = BuildTestVerifier.of(buildGradle, options);
+        assertTrue(verifier.hasDependency("io.micronaut.microstream", "micronaut-microstream", Scope.COMPILE), buildGradle);
+        assertTrue(verifier.hasDependency("io.micronaut.microstream", "micronaut-microstream-annotations", Scope.COMPILE), buildGradle);
+        assertTrue(verifier.hasDependency("io.micronaut.microstream", "micronaut-microstream-annotations", Scope.ANNOTATION_PROCESSOR), buildGradle);
+    }
+
+    @Test
+    void microstreamFeaturesAddsTheLinkInReadmeFile(ProjectGenerator micronautProjectGenerator) throws Exception {
+        Options options = OptionsFixture.defaultGradle().features(List.of("microstream")).build();
+        Map<String, String> project = generateProject(micronautProjectGenerator, options);
+        String readme = project.get("README.md");
+        assertNotNull(readme);
+        assertTrue(readme.contains("https://micronaut-projects.github.io/micronaut-microstream/latest/guide"));
+        assertTrue(readme.contains("https://microstream.one/"));
+    }
+
+    private static Map<String, String> generateProject(ProjectGenerator micronautProjectGenerator,
+                                                       Options options) throws Exception {
+        MapOutputHandler outputHandler = new MapOutputHandler();
+        micronautProjectGenerator.generate(options, outputHandler);
+        return outputHandler.getProject();
+    }
+}
