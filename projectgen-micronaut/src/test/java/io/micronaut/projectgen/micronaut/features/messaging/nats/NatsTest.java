190736e1dde132c1dbec25dd3c723f22ba73afa3
issam liqali <63265720+lissam1@users.noreply.github.com>
2025-05-13 16:35:19 +0100

    nats feature (#138)
    * nats feature
    
    * fix merge
    
    ---------
    
    Co-authored-by: Sergio del Amo <sergio.delamo@softamo.com>

---
+++ projectgen-micronaut/src/test/java/io/micronaut/projectgen/micronaut/features/messaging/nats/NatsTest.java
@@ -0,0 +1,52 @@
+package io.micronaut.projectgen.micronaut.features.messaging.nats;
+
+import io.micronaut.projectgen.core.buildtools.Scope;
+import io.micronaut.projectgen.core.generator.ProjectGenerator;
+import io.micronaut.projectgen.core.io.MapOutputHandler;
+import io.micronaut.projectgen.core.options.Options;
+import io.micronaut.projectgen.micronaut.OptionsFixture;
+import io.micronaut.projectgen.test.BuildTestVerifier;
+import io.micronaut.projectgen.test.ConfigurationUtils;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import java.util.Map;
+import java.util.Properties;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@MicronautTest(startApplication = false)
+class NatsTest {
+    @Test
+    void natsConfiguration(ProjectGenerator micronautProjectGenerator) throws Exception {
+        Options options = OptionsFixture.defaultGradle("nats");
+        Map<String, String> project = generateProject(micronautProjectGenerator, options);
+        Properties applicationProperties = ConfigurationUtils.loadApplicationProperties(project);
+        assertEquals("nats://localhost:4222", applicationProperties.getProperty("nats.default.addresses"));
+    }
+
+    @Test
+    void natsFeaturesAddsTheDependency(ProjectGenerator micronautProjectGenerator) throws Exception {
+        Options options = OptionsFixture.defaultGradle("nats");
+        Map<String, String> project = generateProject(micronautProjectGenerator, options);
+        String buildGradle = project.get("build.gradle.kts");
+        assertNotNull(buildGradle);
+        BuildTestVerifier verifier = BuildTestVerifier.of(buildGradle, options);
+        assertTrue(verifier.hasDependency("io.micronaut.nats", "micronaut-nats", Scope.COMPILE), buildGradle);
+    }
+
+    @Test
+    void natsFeaturesAddsTheLinkInReadmeFile(ProjectGenerator micronautProjectGenerator) throws Exception {
+        Options options = OptionsFixture.defaultGradle("nats");
+        Map<String, String> project = generateProject(micronautProjectGenerator, options);
+        String readme = project.get("README.md");
+        assertNotNull(readme);
+        assertTrue(readme.contains("https://micronaut-projects.github.io/micronaut-nats/snapshot/guide/"));
+    }
+
+    private static Map<String, String> generateProject(ProjectGenerator micronautProjectGenerator,
+                                                       Options options) throws Exception {
+        MapOutputHandler outputHandler = new MapOutputHandler();
+        micronautProjectGenerator.generate(options, outputHandler);
+        return outputHandler.getProject();
+    }
+}
