dc8cb7998e95a7395d2f82cb7c818dcd80d009e1
Sergio del Amo <sergio.delamo@softamo.com>
2025-05-28 15:04:07 +0200

    add FeatureFetcher Test

---
+++ projectgen-core/src/test/java/io/micronaut/projectgen/core/feature/FeatureFetcherTest.java
@@ -0,0 +1,24 @@
+package io.micronaut.projectgen.core.feature;
+
+import io.micronaut.projectgen.core.options.GenericOptionsBuilder;
+import io.micronaut.projectgen.core.options.Options;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@MicronautTest(startApplication = false)
+class FeatureFetcherTest {
+
+    @Test
+    void featureFetcher(FeatureFetcher featureFetcher) {
+        Options options = GenericOptionsBuilder.builder().build();
+        List<FeatureResponse> featureResponse = featureFetcher.fetch(options);
+        assertFalse(featureResponse.isEmpty());
+        List<String> features = featureResponse.stream().map(FeatureResponse::name).toList();
+        assertTrue(features.contains("gitignore"));
+    }
+
+}
