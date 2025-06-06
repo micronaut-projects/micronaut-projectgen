075e39326f086972843908a753a27e6ab7581634
Sergio del Amo <sergio.delamo@softamo.com>
2025-06-06 10:43:49 +0200

    tck should be test scope

--- projectgen-micronaut/src/test/java/io/micronaut/projectgen/micronaut/features/views/ViewsFieldsetTckTest.java
+++ projectgen-micronaut/src/test/java/io/micronaut/projectgen/micronaut/features/views/ViewsFieldsetTckTest.java
@@ -3,6 +3,7 @@ package io.micronaut.projectgen.micronaut.features.views;
 import io.micronaut.projectgen.core.buildtools.Scope;
 import io.micronaut.projectgen.core.generator.ProjectGenerator;
 import io.micronaut.projectgen.core.io.MapOutputHandler;
+import io.micronaut.projectgen.core.io.PreviewGenerator;
 import io.micronaut.projectgen.core.options.GenericOptionsBuilder;
 import io.micronaut.projectgen.core.options.Options;
 import io.micronaut.projectgen.micronaut.ApplicationType;
@@ -19,19 +20,12 @@ import static org.junit.jupiter.api.Assertions.*;
 @MicronautTest(startApplication = false)
 class ViewsFieldsetTckTest {
     @Test
-    void fieldsetTckViewsFeaturesAddsTheDependency(ProjectGenerator micronautProjectGenerator) throws Exception {
+    void fieldsetTckViewsFeaturesAddsTheDependency(PreviewGenerator generator) throws Exception {
         Options options = OptionsFixture.defaultGradle().features(List.of("views-fieldset-tck")).build();
-        Map<String, String> project = generateProject(micronautProjectGenerator, options);
+        Map<String, String> project = generator.generate(options);
         String buildGradle = project.get("build.gradle.kts");
         assertNotNull(buildGradle);
         BuildTestVerifier verifier = BuildTestVerifier.of(buildGradle, options);
         assertTrue(verifier.hasDependency("io.micronaut.views", "micronaut-views-fieldset-tck", Scope.TEST), buildGradle);
-    }
-
-    private static Map<String, String> generateProject(ProjectGenerator micronautProjectGenerator,
-                                                       Options options) throws Exception {
-        MapOutputHandler outputHandler = new MapOutputHandler();
-        micronautProjectGenerator.generate(options, outputHandler);
-        return outputHandler.getProject();
     }
 }
