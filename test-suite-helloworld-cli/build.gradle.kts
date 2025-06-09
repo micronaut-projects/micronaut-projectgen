cbcb0c1bc1b4bb7c75f92a08c501de3d3b7e6f32
Sergio del Amo <sergio.delamo@softamo.com>
2025-06-09 16:47:31 +0200

    fix build

--- test-suite-helloworld-cli/build.gradle.kts
+++ test-suite-helloworld-cli/build.gradle.kts
@@ -6,10 +6,16 @@ version = "0.1"
 group = "io.micronaut.projectgen.demo"
 repositories {
     mavenCentral()
+    maven {
+        url = uri("https://repo.gradle.org/gradle/libs-releases")
+    }
 }
 dependencies {
     annotationProcessor("info.picocli:picocli-codegen")
     implementation(project(":test-suite-helloworld"))
+    implementation(project(":test-suite-helloworld-openrewrite"))
+    implementation(project(":micronaut-projectgen-openrewrite-runner-gradle"))
+    implementation(project(":micronaut-projectgen-openrewrite-runner-maven"))
     implementation("info.picocli:picocli")
     implementation("io.micronaut.picocli:micronaut-picocli")
     runtimeOnly("ch.qos.logback:logback-classic")
