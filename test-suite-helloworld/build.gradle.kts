8e3c16887511c5070839cc1eb148f0145ee2e838
Sergio del Amo <sergio.delamo@softamo.com>
2025-06-09 09:03:21 +0200

    use micronaut-platform version in libs.versions

--- test-suite-helloworld/build.gradle.kts
+++ test-suite-helloworld/build.gradle.kts
@@ -1,22 +1,12 @@
 plugins {
-    `java-library`
+    id("io.micronaut.build.internal.projectgen-test-module")
 }
-repositories {
-    mavenCentral()
-}
+
 dependencies {
-    implementation(platform("io.micronaut.platform:micronaut-platform:${libs.versions.micronaut.platform.get()}"))
     annotationProcessor(mn.micronaut.inject.java)
     testAnnotationProcessor(mn.micronaut.inject.java)
     api(project(":micronaut-projectgen-core"))
     testImplementation(project(":micronaut-projectgen-test"))
     testImplementation(mnTest.micronaut.test.junit5)
     testRuntimeOnly(mnTest.junit.jupiter.engine)
-}
-tasks.withType<Test> {
-    useJUnitPlatform()
-}
-java {
-    sourceCompatibility = JavaVersion.toVersion("17")
-    targetCompatibility = JavaVersion.toVersion("17")
 }
