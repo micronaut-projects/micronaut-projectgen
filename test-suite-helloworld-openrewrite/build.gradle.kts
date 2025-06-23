30380f296553bc75616d1ec34e8b8a5a0471758d
Cedric Champeau <cedric.champeau@gmail.com>
2025-06-23 15:06:06 +0200

    Test suite projects MUST NOT be published
    This fails the Maven Central publication

--- test-suite-helloworld-openrewrite/build.gradle.kts
+++ test-suite-helloworld-openrewrite/build.gradle.kts
@@ -1,22 +1,9 @@
 plugins {
     id("io.micronaut.build.internal.projectgen-test-module")
-    `maven-publish`
 }
+
 dependencies {
     api(project(":micronaut-projectgen-openrewrite"))
     api(project(":test-suite-helloworld"))
     testImplementation(libs.rewrite.test)
 }
-group = "io.micronaut.projectgen"
-version = rootProject.version
-publishing {
-    publications {
-        create<MavenPublication>("maven") {
-            from(components["java"])
-            groupId = project.group.toString()
-            artifactId = project.name
-            version = project.version.toString()
-        }
-    }
-}
-
