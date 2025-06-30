a5b750b37a9248d9d74a87f93986d339982fe782
issam liqali <liqaliissam@gmail.com>
2025-06-30 16:00:40 +0100

    thymeleaf feature

---
+++ test-suite-projectgen-micronaut/src/test/java/io/micronaut/projectgen/micronaut/features/views/ThymeleafTest.java
@@ -0,0 +1,37 @@
+package io.micronaut.projectgen.micronaut.features.views;
+
+import io.micronaut.projectgen.core.buildtools.Scope;
+import io.micronaut.projectgen.core.io.PreviewGenerator;
+import io.micronaut.projectgen.core.options.Options;
+import io.micronaut.projectgen.micronaut.OptionsFixture;
+import io.micronaut.projectgen.test.BuildTestVerifier;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@MicronautTest(startApplication = false)
+class ThymeleafTest {
+    @Test
+    void thymeleafFeaturesAddsTheDependency(PreviewGenerator previewGenerator) throws Exception {
+        Options options = OptionsFixture.defaultGradle().features(List.of("views-thymeleaf")).build();
+        Map<String, String> project = previewGenerator.generate(options);
+        String buildGradle = project.get("build.gradle.kts");
+        assertNotNull(buildGradle);
+        BuildTestVerifier verifier = BuildTestVerifier.of(buildGradle, options);
+        assertTrue(verifier.hasDependency("io.micronaut.views", "micronaut-views-thymeleaf", Scope.COMPILE), buildGradle);
+    }
+
+    @Test
+    void thymeleafFeaturesAddsTheLinkInReadmeFile(PreviewGenerator previewGenerator) throws Exception {
+        Options options = OptionsFixture.defaultGradle().features(List.of("views-thymeleaf")).build();
+        Map<String, String> project = previewGenerator.generate(options);
+        String readme = project.get("README.md");
+        assertNotNull(readme);
+        assertTrue(readme.contains("https://micronaut-projects.github.io/micronaut-views/latest/guide/index.html#thymeleaf"));
+        assertTrue(readme.contains("https://www.thymeleaf.org/"));
+    }
+}
