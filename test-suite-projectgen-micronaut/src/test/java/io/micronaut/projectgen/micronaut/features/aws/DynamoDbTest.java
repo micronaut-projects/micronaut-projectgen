7d574668aa40547910b25e601561de3c4e14e5a8
issam liqali <63265720+lissam1@users.noreply.github.com>
2025-06-30 15:26:38 +0100

    dynamodb feature (#178)

---
+++ test-suite-projectgen-micronaut/src/test/java/io/micronaut/projectgen/micronaut/features/aws/DynamoDbTest.java
@@ -0,0 +1,38 @@
+package io.micronaut.projectgen.micronaut.features.aws;
+
+import io.micronaut.projectgen.core.buildtools.Scope;
+import io.micronaut.projectgen.core.io.PreviewGenerator;
+import io.micronaut.projectgen.core.options.Options;
+import io.micronaut.projectgen.micronaut.OptionsFixture;
+import io.micronaut.projectgen.test.BuildTestVerifier;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@MicronautTest(startApplication = false)
+class DynamoDbTest {
+    @Test
+    void dynamodbFeaturesAddsTheDependency(PreviewGenerator previewGenerator) throws Exception {
+        Options options = OptionsFixture.defaultGradle().features(List.of("dynamodb")).build();
+        Map<String, String> project = previewGenerator.generate(options);
+        String buildGradle = project.get("build.gradle.kts");
+        assertNotNull(buildGradle);
+        BuildTestVerifier verifier = BuildTestVerifier.of(buildGradle, options);
+        assertTrue(verifier.hasDependency("software.amazon.awssdk", "dynamodb", Scope.COMPILE), buildGradle);
+    }
+
+    @Test
+    void dynamodbFeaturesAddsTheLinkInReadmeFile(PreviewGenerator previewGenerator) throws Exception {
+        Options options = OptionsFixture.defaultGradle().features(List.of("dynamodb")).build();
+        Map<String, String> project = previewGenerator.generate(options);
+        String readme = project.get("README.md");
+        assertNotNull(readme);
+        assertTrue(readme.contains("https://micronaut-projects.github.io/micronaut-aws/latest/guide/#dynamodb"));
+        assertTrue(readme.contains("https://aws.amazon.com/dynamodb/"));
+
+    }
+}
