ef2cbc0aa0b9233f96f61673b58683c58d0c4a15
Sergio del Amo <sergio.delamo@softamo.com>
2025-06-30 16:12:23 +0200

    dsiable test

--- test-suite-projectgen-micronaut/src/test/java/io/micronaut/projectgen/micronaut/features/spring/SpringBootTest.java
+++ test-suite-projectgen-micronaut/src/test/java/io/micronaut/projectgen/micronaut/features/spring/SpringBootTest.java
@@ -1,5 +1,6 @@
 package io.micronaut.projectgen.micronaut.features.spring;
 
+import io.micronaut.projectgen.core.buildtools.BuildTool;
 import io.micronaut.projectgen.core.buildtools.Scope;
 import io.micronaut.projectgen.core.io.PreviewGenerator;
 import io.micronaut.projectgen.core.options.Language;
@@ -7,6 +8,7 @@ import io.micronaut.projectgen.core.options.Options;
 import io.micronaut.projectgen.micronaut.OptionsFixture;
 import io.micronaut.projectgen.test.BuildTestVerifier;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -29,14 +31,14 @@ class SpringBootTest {
         assertTrue(verifier.hasDependency("io.micronaut.spring", "micronaut-spring-boot", Scope.RUNTIME), buildGradle);
     }
 
-//Could not get version for ID groovy
-//    @Test
-//    void springBootMavenFeaturesAddsTheDependency(PreviewGenerator previewGenerator) throws Exception {
-//        Options options = OptionsFixture.defaultMaven().language(Language.GROOVY).features(List.of("spring-boot")).build();
-//        Map<String, String> project = previewGenerator.generate(options);
-//        String pom = project.get("pom.xml");
-//        assertNotNull(pom);
-//        BuildTestVerifier verifier = BuildTestVerifier.of(pom, options);
-//        assertTrue(verifier.hasDependency("io.micronaut.spring", "micronaut-spring-boot", Scope.COMPILE), pom);
-//    }
+    @Disabled("Could not get version for ID groovy")
+    @Test
+    void springBootMavenFeaturesAddsTheDependency(PreviewGenerator previewGenerator) throws Exception {
+        Options options = OptionsFixture.defaultMaven().language(Language.GROOVY).features(List.of("spring-boot")).build();
+        Map<String, String> project = previewGenerator.generate(options);
+        String pom = project.get("pom.xml");
+        assertNotNull(pom);
+        BuildTestVerifier verifier = BuildTestVerifier.of(pom, BuildTool.MAVEN, options.language(), options.testFramework());
+        assertTrue(verifier.hasDependency("io.micronaut.spring", "micronaut-spring-boot", Scope.COMPILE), pom);
+    }
 }
