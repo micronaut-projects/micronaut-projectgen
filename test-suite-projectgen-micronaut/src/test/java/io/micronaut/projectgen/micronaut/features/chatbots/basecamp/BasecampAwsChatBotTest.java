4d80d57ce16888776b957d7703be9c24ac5f7348
issam liqali <liqaliissam@gmail.com>
2025-07-01 16:34:23 +0100

    basecamp aws chatbot feature

---
+++ test-suite-projectgen-micronaut/src/test/java/io/micronaut/projectgen/micronaut/features/chatbots/basecamp/BasecampAwsChatBotTest.java
@@ -0,0 +1,38 @@
+package io.micronaut.projectgen.micronaut.features.chatbots.basecamp;
+
+import io.micronaut.projectgen.core.buildtools.Scope;
+import io.micronaut.projectgen.core.io.PreviewGenerator;
+import io.micronaut.projectgen.core.options.Options;
+import io.micronaut.projectgen.micronaut.OptionsFixture;
+import io.micronaut.projectgen.test.BuildTestVerifier;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@MicronautTest(startApplication = false)
+class BasecampAwsChatBotTest {
+    @Test
+    void basecampAwsChatbotFeaturesAddsTheDependency(PreviewGenerator previewGenerator) throws Exception {
+        Options options = OptionsFixture.defaultGradle().template("function").features(List.of("chatbots-basecamp-lambda")).build();
+        Map<String, String> project = previewGenerator.generate(options);
+        String buildGradle = project.get("build.gradle.kts");
+        assertNotNull(buildGradle);
+        BuildTestVerifier verifier = BuildTestVerifier.of(buildGradle, options);
+        assertTrue(verifier.hasDependency("io.micronaut.chatbots", "micronaut-chatbots-basecamp-lambda", Scope.COMPILE), buildGradle);
+    }
+
+    @Test
+    void basecampAwsChatbotFeaturesAddsTheLinkInReadmeFile(PreviewGenerator previewGenerator) throws Exception {
+        Options options = OptionsFixture.defaultGradle().template("function").features(List.of("chatbots-basecamp-lambda")).build();
+        Map<String, String> project = previewGenerator.generate(options);
+        String readme = project.get("README.md");
+        assertNotNull(readme);
+        assertTrue(readme.contains("https://micronaut-projects.github.io/micronaut-chatbots/latest/guide/"));
+        assertTrue(readme.contains("https://github.com/basecamp/bc3-api/blob/master/sections/chatbots.md"));
+
+    }
+}
