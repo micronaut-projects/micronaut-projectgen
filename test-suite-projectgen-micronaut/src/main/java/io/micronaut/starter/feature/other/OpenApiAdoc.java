8cb8d2549c286c1c5ad42cecd5fd329a9feab2ab
issam liqali <63265720+lissam1@users.noreply.github.com>
2025-07-02 12:51:00 +0100

    Other features (#188)

--- test-suite-projectgen-micronaut/src/main/java/io/micronaut/starter/feature/other/OpenApiAdoc.java
+++ test-suite-projectgen-micronaut/src/main/java/io/micronaut/starter/feature/other/OpenApiAdoc.java
@@ -18,6 +18,7 @@ package io.micronaut.starter.feature.other;
 import io.micronaut.context.annotation.Requires;
 import io.micronaut.core.util.StringUtils;
 import io.micronaut.projectgen.core.generator.ModuleContext;
+import io.micronaut.projectgen.core.openrewrite.OpenRewriteFeature;
 import io.micronaut.projectgen.core.options.Options;
 import io.micronaut.projectgen.micronaut.ApplicationType;
 import io.micronaut.projectgen.core.generator.GeneratorContext;
@@ -29,17 +30,13 @@ import io.micronaut.projectgen.core.feature.FeatureContext;
 import io.micronaut.starter.feature.server.MicronautServerDependent;
 import jakarta.inject.Singleton;
 
+import java.util.List;
+
 @Requires(property = "micronaut.starter.feature.openapi.adoc.enabled", value = StringUtils.TRUE, defaultValue = StringUtils.TRUE)
 @Singleton
-public class OpenApiAdoc implements Feature, MicronautServerDependent {
+public class OpenApiAdoc implements OpenRewriteFeature, MicronautServerDependent {
 
     public static final String NAME = "openapi-adoc";
-
-    private static final Dependency DEPENDENCY_OPENAPI_ASCIIDOC = MicronautDependencyUtils
-            .openapi()
-            .artifactId("micronaut-openapi-adoc")
-            .annotationProcessor()
-            .build();
 
     private final OpenApi openApiFeature;
 
@@ -63,12 +60,6 @@ public class OpenApiAdoc implements Feature, MicronautServerDependent {
     }
 
     @Override
-    public void apply(GeneratorContext generatorContext) {
-        ModuleContext module = generatorContext.getRootModule();
-        module.addDependency(DEPENDENCY_OPENAPI_ASCIIDOC);
-    }
-
-    @Override
     public void processSelectedFeatures(FeatureContext featureContext) {
         featureContext.addFeatureIfNotPresent(OpenApi.class, openApiFeature);
     }
@@ -83,4 +74,10 @@ public class OpenApiAdoc implements Feature, MicronautServerDependent {
     public String getCategory() {
         return Category.API;
     }
+
+    @Override
+    public List<String> getRecipes(GeneratorContext generatorContext) {
+        return List.of("io.micronaut.starter.feature.openapi-adoc");
+    }
+
 }
---
+++ test-suite-projectgen-micronaut/src/test/java/io/micronaut/projectgen/micronaut/features/other/OpenApiTest.java
@@ -0,0 +1,52 @@
+package io.micronaut.projectgen.micronaut.features.other;
+
+import io.micronaut.projectgen.core.buildtools.Scope;
+import io.micronaut.projectgen.core.io.PreviewGenerator;
+import io.micronaut.projectgen.core.options.Language;
+import io.micronaut.projectgen.core.options.Options;
+import io.micronaut.projectgen.micronaut.OptionsFixture;
+import io.micronaut.projectgen.test.BuildTestVerifier;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@MicronautTest(startApplication = false)
+class OpenApiTest {
+    @Test
+    void openapiFeaturesAddsTheDependency(PreviewGenerator previewGenerator) throws Exception {
+        Options options = OptionsFixture.defaultGradle().features(List.of("openapi")).build();
+        Map<String, String> project = previewGenerator.generate(options);
+        String buildGradle = project.get("build.gradle.kts");
+        assertNotNull(buildGradle);
+        BuildTestVerifier verifier = BuildTestVerifier.of(buildGradle, options);
+        assertTrue(verifier.hasDependency("io.micronaut.openapi", "micronaut-openapi-annotations", Scope.COMPILE_ONLY), buildGradle);
+        assertTrue(verifier.hasDependency("io.micronaut.openapi", "micronaut-openapi", Scope.ANNOTATION_PROCESSOR), buildGradle);
+    }
+
+    @Disabled
+    @Test
+    void openapiMavenFeaturesAddsTheDependency(PreviewGenerator previewGenerator) throws Exception {
+        Options options = OptionsFixture.defaultMaven().language(Language.GROOVY).features(List.of("openapi")).build();
+        Map<String, String> project = previewGenerator.generate(options);
+        String pom = project.get("pom.xml");
+        assertNotNull(pom);
+        BuildTestVerifier verifier = BuildTestVerifier.of(pom, options);
+        assertTrue(verifier.hasDependency("io.micronaut.openapi", "micronaut-openapi"), pom);
+    }
+
+    @Test
+    void openapiFeaturesAddsTheLinkInReadmeFile(PreviewGenerator previewGenerator) throws Exception {
+        Options options = OptionsFixture.defaultGradle().features(List.of("openapi")).build();
+        Map<String, String> project = previewGenerator.generate(options);
+        String readme = project.get("README.md");
+        assertNotNull(readme);
+        assertTrue(readme.contains("https://micronaut-projects.github.io/micronaut-openapi/latest/guide/index.html"));
+        assertTrue(readme.contains("https://www.openapis.org"));
+
+    }
+}
