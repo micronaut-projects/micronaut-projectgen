6eed3090237edb8901bbfb5cbd4b644b1a0c9eb3
Sergio del Amo <sergio.delamo@softamo.com>
2025-07-08 10:08:32 +0200

    failing tests

--- test-suite-projectgen-micronaut/src/test/java/io/micronaut/projectgen/micronaut/features/database/jdbc/HikariTest.java
+++ test-suite-projectgen-micronaut/src/test/java/io/micronaut/projectgen/micronaut/features/database/jdbc/HikariTest.java
@@ -17,17 +17,73 @@ import static org.junit.jupiter.api.Assertions.*;
 
 @MicronautTest(startApplication = false)
 class HikariTest {
+    private static final String DATASOURCES_DEFAULT_DB_TYPE = "datasources.default.db-type";
+    private static final String DATASOURCES_DEFAULT_URL = "datasources.default.url";
+    private static final String DATASOURCES_DEFAULT_DRIVER_CLASS_NAME = "datasources.default.driver-class-name";
+    private static final String DATASOURCES_DEFAULT_USERNAME = "datasources.default.username";
+    private static final String DATASOURCES_DEFAULT_PASSWORD = "datasources.default.password";
+
     @Test
-    void hikariFeaturesConfiguration(PreviewGenerator previewGenerator) throws Exception {
+    void h2DriverUsedByDefaultWhenAddingJdbcHikari(PreviewGenerator previewGenerator) throws Exception {
         Options options = OptionsFixture.defaultGradle().features(List.of("jdbc-hikari")).build();
         Map<String, String> project = previewGenerator.generate(options);
         Properties applicationProperties = ConfigurationUtils.loadApplicationProperties(project);
-        String buildGradle = project.get("build.gradle.kts");
-        assertNotNull(buildGradle);
-        assertEquals("jdbc:h2:mem:devDb;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE", applicationProperties.getProperty("datasources.default.url"));
-        assertEquals("org.h2.Driver", applicationProperties.getProperty("datasources.default.driver-class-name"));
-        assertEquals("sa", applicationProperties.getProperty("datasources.default.username"));
-        assertEquals("", applicationProperties.getProperty("datasources.default.password"));
+        assertEquals("jdbc:h2:mem:devDb;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE", applicationProperties.getProperty(DATASOURCES_DEFAULT_URL));
+        assertEquals("org.h2.Driver", applicationProperties.getProperty(DATASOURCES_DEFAULT_DRIVER_CLASS_NAME));
+        assertEquals("sa", applicationProperties.getProperty(DATASOURCES_DEFAULT_USERNAME));
+        assertEquals("", applicationProperties.getProperty(DATASOURCES_DEFAULT_PASSWORD));
+    }
+
+    @Test
+    void postgresqlWhenAddingJdbcHikari(PreviewGenerator previewGenerator) throws Exception {
+        Options options = OptionsFixture.defaultGradle().features(List.of("jdbc-hikari", "postgres")).build();
+        Map<String, String> project = previewGenerator.generate(options);
+        Properties applicationProperties = ConfigurationUtils.loadApplicationProperties(project);
+        assertEquals("postgres", applicationProperties.getProperty(DATASOURCES_DEFAULT_DB_TYPE));
+        assertEquals("org.postgresql.Driver", applicationProperties.getProperty(DATASOURCES_DEFAULT_DRIVER_CLASS_NAME));
+        assertNull(applicationProperties.getProperty(DATASOURCES_DEFAULT_USERNAME));
+        assertNull(applicationProperties.getProperty(DATASOURCES_DEFAULT_PASSWORD));
+    }
+
+    @Test
+    void mysqlWhenAddingJdbcHikari(PreviewGenerator previewGenerator) throws Exception {
+        Options options = OptionsFixture.defaultGradle().features(List.of("jdbc-hikari", "mysql")).build();
+        Map<String, String> project = previewGenerator.generate(options);
+        Properties applicationProperties = ConfigurationUtils.loadApplicationProperties(project);
+        assertEquals("mysql", applicationProperties.getProperty(DATASOURCES_DEFAULT_DB_TYPE));
+        assertEquals("com.mysql.cj.jdbc.Driver", applicationProperties.getProperty(DATASOURCES_DEFAULT_DRIVER_CLASS_NAME));
+        assertNull(applicationProperties.getProperty(DATASOURCES_DEFAULT_USERNAME));
+        assertNull(applicationProperties.getProperty(DATASOURCES_DEFAULT_PASSWORD));
+    }
+
+    @Test
+    void oracleWhenAddingJdbcHikari(PreviewGenerator previewGenerator) throws Exception {
+        Options options = OptionsFixture.defaultGradle().features(List.of("jdbc-hikari", "oracle")).build();
+        Map<String, String> project = previewGenerator.generate(options);
+        Properties applicationProperties = ConfigurationUtils.loadApplicationProperties(project);
+        assertEquals("oracle", applicationProperties.getProperty(DATASOURCES_DEFAULT_DB_TYPE));
+        assertEquals("oracle.jdbc.OracleDriver", applicationProperties.getProperty(DATASOURCES_DEFAULT_DRIVER_CLASS_NAME));
+        assertNull(applicationProperties.getProperty(DATASOURCES_DEFAULT_USERNAME));
+        assertNull(applicationProperties.getProperty(DATASOURCES_DEFAULT_PASSWORD));
+    }
+
+    @Test
+    void sqlserverWhenAddingJdbcHikari(PreviewGenerator previewGenerator) throws Exception {
+        Options options = OptionsFixture.defaultGradle().features(List.of("jdbc-hikari", "sqlserver")).build();
+        Map<String, String> project = previewGenerator.generate(options);
+        Properties applicationProperties = ConfigurationUtils.loadApplicationProperties(project);
+        assertEquals("mssql", applicationProperties.getProperty(DATASOURCES_DEFAULT_DB_TYPE));
+        assertEquals("com.microsoft.sqlserver.jdbc.SQLServerDriver", applicationProperties.getProperty(DATASOURCES_DEFAULT_DRIVER_CLASS_NAME));
+        assertEquals("false", applicationProperties.getProperty("test-resources.containers.mssql.accept-license"));
+    }
+
+    @Test
+    void mariadbWhenAddingJdbcHikari(PreviewGenerator previewGenerator) throws Exception {
+        Options options = OptionsFixture.defaultGradle().features(List.of("jdbc-hikari", "mariadb")).build();
+        Map<String, String> project = previewGenerator.generate(options);
+        Properties applicationProperties = ConfigurationUtils.loadApplicationProperties(project);
+        assertEquals("mariadb", applicationProperties.getProperty(DATASOURCES_DEFAULT_DB_TYPE));
+        assertEquals("org.mariadb.jdbc.Driver", applicationProperties.getProperty(DATASOURCES_DEFAULT_DRIVER_CLASS_NAME));
     }
 
     @Test
