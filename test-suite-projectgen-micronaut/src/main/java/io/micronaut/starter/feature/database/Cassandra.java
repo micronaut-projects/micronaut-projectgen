35b82253d48be4038693dc5f2e428f4e5cfd23de
issam liqali <liqaliissam@gmail.com>
2025-07-09 10:40:58 +0100

    add cassandra configurations

--- test-suite-projectgen-micronaut/src/main/java/io/micronaut/starter/feature/database/Cassandra.java
+++ test-suite-projectgen-micronaut/src/main/java/io/micronaut/starter/feature/database/Cassandra.java
@@ -20,6 +20,7 @@ import io.micronaut.core.util.StringUtils;
 import io.micronaut.projectgen.core.generator.ModuleContext;
 import io.micronaut.projectgen.core.generator.GeneratorContext;
 import io.micronaut.projectgen.core.buildtools.dependencies.Dependency;
+import io.micronaut.projectgen.core.openrewrite.OpenRewriteFeature;
 import io.micronaut.starter.buildtools.dependencies.MicronautDependencyUtils;
 import io.micronaut.starter.feature.Category;
 import io.micronaut.projectgen.core.feature.Feature;
@@ -29,26 +30,20 @@ import io.micronaut.starter.feature.testcontainers.ContributingTestContainerDepe
 import io.micronaut.starter.feature.testresources.TestResources;
 import jakarta.inject.Singleton;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
 @Requires(property = "micronaut.starter.feature.cassandra.enabled", value = StringUtils.TRUE, defaultValue = StringUtils.TRUE)
 @Singleton
-public class Cassandra implements Feature, ContributingTestContainerDependency {
+public class Cassandra implements OpenRewriteFeature, ContributingTestContainerDependency {
 
     public static final String NAME = "cassandra";
-
-    private static final Dependency MICRONAUT_CASSANDRA_DEPENDENCY = MicronautDependencyUtils.cassandraDependency()
-            .artifactId("micronaut-cassandra")
-            .compile()
-            .build();
-
-    private static final Dependency CASSANDRA_METRICS_MICROMETER_DEPENDENCY = Dependency.builder()
-            .groupId("com.datastax.oss")
-            .artifactId("java-driver-metrics-micrometer")
-            .compile()
-            .build();
+    private static final String CASSANDRA_RECIPE = "io.micronaut.starter.feature.cassandra";
+    private static final String CASSANDRA_RECIPE_CONFIG_MICROMETER = "io.micronaut.starter.feature.cassandra-config-micrometer";
+    private static final String CASSANDRA_RECIPE_CONFIG_TESTRESOURCES = "io.micronaut.starter.feature.cassandra-config-testresources";
+    private static final String CASSANDRA_RECIPE_CONFIG = "io.micronaut.starter.feature.cassandra-config";
 
     @Override
     public String getName() {
@@ -66,55 +61,23 @@ public class Cassandra implements Feature, ContributingTestContainerDependency {
     }
 
     @Override
-    public void apply(GeneratorContext generatorContext) {
-        applyDependencies(generatorContext);
-        applyConfiguration(generatorContext);
-    }
-
-    private void applyDependencies(GeneratorContext generatorContext) {
-        ModuleContext module = generatorContext.getRootModule();
-        module.addDependency(MICRONAUT_CASSANDRA_DEPENDENCY);
-        if (generatorContext.isFeaturePresent(MicrometerFeature.class)) {
-            module.addDependency(CASSANDRA_METRICS_MICROMETER_DEPENDENCY);
-        }
-    }
-
-    private void applyConfiguration(GeneratorContext generatorContext) {
-        ModuleContext module = generatorContext.getRootModule();
-        ApplicationConfiguration configuration = module.configuration();
-        configuration.put("cassandra.default.basic.load-balancing-policy.local-datacenter", "datacenter1");
-        configuration.put("cassandra.default.basic.session-name", "defaultSession");
-        configuration.put("cassandra.default.advanced.control-connection.schema-agreement.timeout", 20);
-        configuration.put("cassandra.default.advanced.ssl-engine-factory", "DefaultSslEngineFactory");
-
-        if (generatorContext.isFeaturePresent(MicrometerFeature.class)) {
-            configuration.put("cassandra.default.advanced.metrics.factory.class", "MicrometerMetricsFactory");
-            configuration.put("cassandra.default.advanced.metrics.session.enabled", List.of("connected-nodes", "cql-requests", "bytes-sent", "bytes-received"));
-            configuration.put("cassandra.default.advanced.metrics.node.enabled", List.of("cql-requests", "bytes-sent", "bytes-received"));
-        }
-        if (generatorContext.isFeaturePresent(TestResources.class)) {
-            configuration.put("test-resources.containers.cassandra.startup-timeout", "600s");
-            configuration.put("test-resources.containers.cassandra.image-name", "cassandra");
-            configuration.put("test-resources.containers.cassandra.exposed-ports", List.of(Map.of("cassandra.port", 9042)));
-            configuration.put("cassandra.default.basic.contact-points", List.of("localhost:${cassandra.port}"));
-        } else {
-            configuration.put("cassandra.default.basic.contact-points", List.of("localhost:9042"));
-        }
-    }
-
-    @Override
     public String getCategory() {
         return Category.DATABASE;
     }
 
     @Override
-    public String getFrameworkDocumentation(GeneratorContext generatorContext) {
-        return "https://micronaut-projects.github.io/micronaut-cassandra/latest/guide/index.html";
-    }
-
-    @Override
-    public String getThirdPartyDocumentation(GeneratorContext generatorContext) {
-        return "https://docs.datastax.com/en/developer/java-driver/latest/";
+    public List<String> getRecipes(GeneratorContext generatorContext) {
+        List<String> recipes = new ArrayList<>();
+        recipes.add(CASSANDRA_RECIPE);
+        if (generatorContext.isFeaturePresent(MicrometerFeature.class)) {
+            recipes.add(CASSANDRA_RECIPE_CONFIG_MICROMETER);
+        }
+        if (generatorContext.isFeaturePresent(TestResources.class)) {
+            recipes.add(CASSANDRA_RECIPE_CONFIG_TESTRESOURCES);
+        } else {
+            recipes.add(CASSANDRA_RECIPE_CONFIG);
+        }
+            return recipes;
     }
 
     @Override
