36f7396d47fd5c7379b9d8a373260e0d278a6720
issam liqali <liqaliissam@gmail.com>
2025-07-14 15:08:58 +0100

    swagger ui docs with openrewrite

--- test-suite-projectgen-micronaut/src/main/java/io/micronaut/starter/feature/other/SwaggerUI.java
+++ test-suite-projectgen-micronaut/src/main/java/io/micronaut/starter/feature/other/SwaggerUI.java
@@ -18,6 +18,7 @@ package io.micronaut.starter.feature.other;
 import io.micronaut.context.annotation.Requires;
 import io.micronaut.core.util.StringUtils;
 import io.micronaut.projectgen.core.generator.GeneratorContext;
+import io.micronaut.projectgen.core.openrewrite.OpenRewriteFeature;
 import io.micronaut.starter.feature.ContributingInterceptUrlMapFeature;
 import io.micronaut.starter.feature.InterceptUrlMap;
 import io.micronaut.starter.feature.staticResources.StaticResource;
@@ -28,7 +29,7 @@ import java.util.List;
 
 @Requires(property = "micronaut.starter.feature.swagger.ui.enabled", value = StringUtils.TRUE, defaultValue = StringUtils.TRUE)
 @Singleton
-public class SwaggerUI extends OpenApiView implements ContributingInterceptUrlMapFeature {
+public class SwaggerUI extends OpenApiView implements ContributingInterceptUrlMapFeature, OpenRewriteFeature {
     public static final String NAME = "swagger-ui";
 
     public SwaggerUI(OpenApi openApiFeature) {
@@ -51,13 +52,8 @@ public class SwaggerUI extends OpenApiView implements ContributingInterceptUrlMa
     }
 
     @Override
-    public String getThirdPartyDocumentation(GeneratorContext generatorContext) {
-        return "https://swagger.io/tools/swagger-ui/";
-    }
-
-    @Override
-    public String getFrameworkDocumentation(GeneratorContext generatorContext) {
-        return "https://micronaut-projects.github.io/micronaut-openapi/latest/guide/index.html";
+    public List<String> getRecipes(GeneratorContext generatorContext) {
+        return List.of("io.micronaut.starter.feature.swagger-ui-docs");
     }
 
     @Override
---
+++ test-suite-projectgen-micronaut/src/test/java/io/micronaut/projectgen/micronaut/features/other/SwaggerUITest.java
@@ -0,0 +1,25 @@
+package io.micronaut.projectgen.micronaut.features.other;
+
+import io.micronaut.projectgen.core.io.PreviewGenerator;
+import io.micronaut.projectgen.core.options.Options;
+import io.micronaut.projectgen.micronaut.OptionsFixture;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@MicronautTest(startApplication = false)
+class SwaggerUITest {
+    @Test
+    void swaggerUiFeaturesAddsTheLinkInReadmeFile(PreviewGenerator previewGenerator) throws Exception {
+        Options options = OptionsFixture.defaultGradle().features(List.of("swagger-ui")).build();
+        Map<String, String> project = previewGenerator.generate(options);
+        String readme = project.get("README.md");
+        assertNotNull(readme);
+        assertTrue(readme.contains("https://micronaut-projects.github.io/micronaut-openapi/latest/guide/index.html"));
+        assertTrue(readme.contains("https://swagger.io/tools/swagger-ui/"));
+    }
+}
