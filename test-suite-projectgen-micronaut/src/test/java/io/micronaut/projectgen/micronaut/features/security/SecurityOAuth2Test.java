0fb6a58be21552189724076f1f973cfe526d760b
issam liqali <liqaliissam@gmail.com>
2025-07-14 16:21:30 +0100

    add security oauth2 config recipe

--- test-suite-projectgen-micronaut/src/test/java/io/micronaut/projectgen/micronaut/features/security/SecurityOAuth2Test.java
+++ test-suite-projectgen-micronaut/src/test/java/io/micronaut/projectgen/micronaut/features/security/SecurityOAuth2Test.java
@@ -22,9 +22,22 @@ class SecurityOAuth2Test {
         Options options = OptionsFixture.defaultGradle().features(List.of("security-oauth2")).build();
         Map<String, String> project = previewGenerator.generate(options);
         Properties applicationProperties = ConfigurationUtils.loadApplicationProperties(project);
+        Properties applicationDevProperties = ConfigurationUtils.loadDevProperties(project);
         String buildGradle = project.get("build.gradle.kts");
         assertNotNull(buildGradle);
         assertEquals("cookie", applicationProperties.getProperty("micronaut.security.authentication"));
+        assertEquals("XXX", applicationDevProperties.getProperty("micronaut.security.oauth2.clients.default.client-id"));
+        assertEquals("XXX", applicationDevProperties.getProperty("micronaut.security.oauth2.clients.default.client-id"));
+    }
+
+    @Test
+    void securityOauth2JwtFeaturesConfiguration(PreviewGenerator previewGenerator) throws Exception {
+        Options options = OptionsFixture.defaultGradle().features(List.of("security-oauth2", "security-jwt")).build();
+        Map<String, String> project = previewGenerator.generate(options);
+        Properties applicationDevProperties = ConfigurationUtils.loadDevProperties(project);
+        String buildGradle = project.get("build.gradle.kts");
+        assertNotNull(buildGradle);
+        assertEquals("${OAUTH_ISSUER}", applicationDevProperties.getProperty("micronaut.security.oauth2.clients.default.openid.issuer"));
     }
 
     @Test
