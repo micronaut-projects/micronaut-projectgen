75f245625f97f01be430111c1e98f132594b5a34
issam liqali <liqaliissam@gmail.com>
2025-07-17 12:43:28 +0100

    add test for legacyBuildToolName method

--- test-suite-projectgen-micronaut/src/test/java/io/micronaut/projectgen/micronaut/features/cli/MicronautCliTest.java
+++ test-suite-projectgen-micronaut/src/test/java/io/micronaut/projectgen/micronaut/features/cli/MicronautCliTest.java
@@ -1,12 +1,36 @@
 package io.micronaut.projectgen.micronaut.features.cli;
 
+import io.micronaut.projectgen.core.buildtools.gradle.GradleDsl;
+import io.micronaut.projectgen.core.options.Options;
+import io.micronaut.projectgen.micronaut.OptionsFixture;
 import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class MicronautCliTest {
 
     @Test
     void testLegacyBuildToolName() {
-        //TODO write test for {@link MicronautCli#legacyBuildToolName}
+        // Test Gradle Kotlin DSL
+        Options gradleKotlinDsl = OptionsFixture.defaultGradle().gradleDsl(GradleDsl.KOTLIN).build();
+        Optional<String> gradleKotlin = MicronautCli.legacyBuildToolName(gradleKotlinDsl);
+        assertTrue(gradleKotlin.isPresent());
+        assertEquals(MicronautCli.LEGACY_BUILD_TOOL_GRADLE_KOTLIN, gradleKotlin.get());
+
+        // Test Gradle Groovy DSL
+        Options gradleGroovyDsl = OptionsFixture.defaultGradle().gradleDsl(GradleDsl.GROOVY).build();
+        Optional<String> gradleGroovy = MicronautCli.legacyBuildToolName(gradleGroovyDsl);
+        assertTrue(gradleGroovy.isPresent());
+        assertEquals(MicronautCli.LEGACY_BUILD_TOOL_GRADLE_GROOVY, gradleGroovy.get());
+
+        //Test Maven
+        Options mavenBuild = OptionsFixture.defaultMaven().build();
+        Optional<String> maven = MicronautCli.legacyBuildToolName(mavenBuild);
+        assertTrue(maven.isPresent());
+        assertEquals(MicronautCli.LEGACY_BUILD_TOOL_MAVEN, maven.get());
     }
 
     @Test
