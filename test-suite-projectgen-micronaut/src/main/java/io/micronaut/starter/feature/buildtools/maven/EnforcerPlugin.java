6ba7c4ee4c254edd7ffe5d6764d30494970806ea
issam liqali <63265720+lissam1@users.noreply.github.com>
2025-07-24 15:42:43 +0100

    maven enforcer plugin feature support openrewrite (#282)

--- test-suite-projectgen-micronaut/src/main/java/io/micronaut/starter/feature/buildtools/maven/EnforcerPlugin.java
+++ test-suite-projectgen-micronaut/src/main/java/io/micronaut/starter/feature/buildtools/maven/EnforcerPlugin.java
@@ -20,6 +20,7 @@ import io.micronaut.core.annotation.NonNull;
 import io.micronaut.core.annotation.Nullable;
 import io.micronaut.core.util.StringUtils;
 import io.micronaut.projectgen.core.generator.ModuleContext;
+import io.micronaut.projectgen.core.openrewrite.OpenRewriteFeature;
 import io.micronaut.projectgen.core.utils.OptionUtils;
 import io.micronaut.projectgen.core.generator.GeneratorContext;
 import io.micronaut.projectgen.core.buildtools.maven.MavenPlugin;
@@ -29,13 +30,12 @@ import io.micronaut.projectgen.core.feature.Feature;
 import io.micronaut.projectgen.core.options.Options;
 import jakarta.inject.Singleton;
 
+import java.util.List;
 import java.util.Set;
 
 @Requires(property = "micronaut.starter.feature.maven.enforcer.plugin.enabled", value = StringUtils.TRUE, defaultValue = StringUtils.TRUE)
 @Singleton
-public class EnforcerPlugin implements DefaultFeature {
-    private static final String GROUP_ID_ORG_APACHE_MAVEN_PLUGINS = "org.apache.maven.plugins";
-    private static final String ARTIFACT_ID_MAVEN_ENFORCER_PLUGIN = "maven-enforcer-plugin";
+public class EnforcerPlugin implements DefaultFeature, OpenRewriteFeature {
     private static final String NAME = "maven-enforcer-plugin";
 
     @Override
@@ -66,21 +66,8 @@ public class EnforcerPlugin implements DefaultFeature {
     }
 
     @Override
-    public @Nullable String getThirdPartyDocumentation(GeneratorContext generatorContext) {
-        return "https://maven.apache.org/enforcer/maven-enforcer-plugin/";
-    }
-
-    @Override
-    public void apply(GeneratorContext generatorContext) {
-        addEnforcerPlugin(generatorContext);
-    }
-
-    protected void addEnforcerPlugin(GeneratorContext generatorContext) {
-        ModuleContext module = generatorContext.getRootModule();
-        module.addBuildPlugin(MavenPlugin.builder()
-                .groupId(GROUP_ID_ORG_APACHE_MAVEN_PLUGINS)
-                .artifactId(ARTIFACT_ID_MAVEN_ENFORCER_PLUGIN)
-                .build());
+    public List<String> getRecipes(GeneratorContext generatorContext) {
+        return List.of("io.micronaut.starter.feature.maven-enforcer-plugin");
     }
 
     @Override
